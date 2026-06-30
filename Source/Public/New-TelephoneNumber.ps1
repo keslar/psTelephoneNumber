99cd3e99cf97519114379dc177eacfe74cfdb87e
[. Keslar crk4@pitt.edu <crk4@pitt.edu>
2026-06-29 21:06:26 -0400

    fix: remove redundant regex pre-check in New-TelephoneNumber
    The regex was both too restrictive (rejected valid characters like /)
    and unnecessary since TelephoneNumber.Parse() does full validation.

--- Source/Public/New-TelephoneNumber.ps1
+++ Source/Public/New-TelephoneNumber.ps1
@@ -24,9 +24,5 @@ function New-TelephoneNumber {
         [Parameter(Mandatory)]
         [string]$Number
     )
-    # Validate the telephone number format (basic validation)
-    if ($Number -notmatch '^\+?[0-9\s\-\(\)\.]+$') {
-        throw 'Invalid telephone number format. Please provide a valid number.'
-    }
     return [TelephoneNumber]::new($Number)
 }
